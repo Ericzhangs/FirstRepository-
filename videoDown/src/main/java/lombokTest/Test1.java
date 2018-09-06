e0cb301a516fd4b8c9222f5d804e2e259131ed5d
Ericzhangs <1316463622@qq.com>
2018-09-06 16:54:10 +0800

    set pom

--- videoDown/src/main/java/lombokTest/Test1.java
+++ videoDown/src/main/java/lombokTest/Test1.java
@@ -3,6 +3,8 @@ package lombokTest;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.Date;
+
 @Slf4j
 @Data
 public class Test1 {
@@ -10,14 +12,13 @@ public class Test1 {
     private String name;
     private int age;
 
-//    private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(LogExample.class);
+
+    public void getLog(){
+        log.info("呵呵呵呵呵呵");
+    }
 
     public static void main(String[] args) {
         Test1 t1 = new Test1();
-        t1.setName("nnn");
-        System.out.println(t1.getName());
-
-        log
-
+        t1.getLog();
     }
 }
